0cc87806cddd8f107c4073f973698b9648afdd47
JaakkoGee <131030750+JageTheProgrammer@users.noreply.github.com>
2025-06-28 17:23:32 +0300

    Update index.js

--- backend/socket/index.js
+++ backend/socket/index.js
@@ -1,1 +1,34 @@
+const { Server } = require("socket.io");
 
+let onlineUsers = {};
+
+function setupSocket(server) {
+  const io = new Server(server, {
+    cors: { origin: "*" },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("User connected:", socket.id);
+
+    socket.on("user-connected", (userId) => {
+      onlineUsers[userId] = socket.id;
+    });
+
+    socket.on("send-message", ({ to, message }) => {
+      const receiverSocket = onlineUsers[to];
+      if (receiverSocket) {
+        io.to(receiverSocket).emit("receive-message", message);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      for (let userId in onlineUsers) {
+        if (onlineUsers[userId] === socket.id) {
+          delete onlineUsers[userId];
+        }
+      }
+    });
+  });
+}
+
+module.exports = { setupSocket };
