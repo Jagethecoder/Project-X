db288dab11e9d6b6261ea0b942f66d522fe50465
JaakkoGee <131030750+JageTheProgrammer@users.noreply.github.com>
2025-06-28 17:22:15 +0300

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  friends: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  avatar: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
