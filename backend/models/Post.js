77661d8414e1ed6b3368f1d1c8161d37b83c2783
JaakkoGee <131030750+JageTheProgrammer@users.noreply.github.com>
2025-06-28 17:23:01 +0300

    Update Post.js

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,1 +1,16 @@
+const mongoose = require('mongoose');
 
+const postSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  mediaUrl: String,
+  caption: String,
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [
+    {
+      user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      text: String
+    }
+  ]
+}, { timestamps: true });
+
+module.exports = mongoose.model('Post', postSchema);
