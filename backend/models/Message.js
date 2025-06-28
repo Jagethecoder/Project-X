c893c2350bf62a7ec4c8fde86334bd26328929d5
JaakkoGee <131030750+JageTheProgrammer@users.noreply.github.com>
2025-06-28 17:22:34 +0300

    Update Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
 
+const messageSchema = new mongoose.Schema({
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  receiver: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  text: String,
+  voiceUrl: String,
+}, { timestamps: true });
+
+module.exports = mongoose.model('Message', messageSchema);
