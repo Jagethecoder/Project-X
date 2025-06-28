3580f79cc927923a567c95ff101b7a2ec4cf3164
Jagethecoder <jaakkohtml@gmail.com>
2025-06-28 18:38:29 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,11 @@ const http = require('http');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
-const { setupSocket } = require('./socket');
+const { setupSocket } = require('./socket'); // <- this must match your folder name
+
 const app = express();
 dotenv.config();
-
-connectDB(); // connect MongoDB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -19,7 +19,7 @@ app.use('/api/messages', require('./routes/messages'));
 app.use('/api/posts', require('./routes/posts'));
 
 const server = http.createServer(app);
-setupSocket(server);
+setupSocket(server); // <--- Socket.IO runs on this server
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
